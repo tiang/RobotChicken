ea1d95d68b30156332039c84f3a38621aabb2821
Tiang Cheng <tiang_1@hotmail.com>
2015-05-18 23:58:06 +1000

    Add left and right methods

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,16 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
+var directionMatrix = {
+    'n': { x: 0, y: 1 },
+    'e': { x: 1, y: 0 },
+    's': { x: 0, y: -1 },
+    'w': { x: -1, y: 0 }
+};
+
+//todo: make this an iterator
+var directionArray = ['n', 'e', 's', 'w'];
+
 var Robot = (function () {
     function Robot(x, y, direction) {
         _classCallCheck(this, Robot);
@@ -13,16 +23,12 @@ var Robot = (function () {
 
         this.direction = direction.charAt(0);
 
-        this.directionMatrix = {
-            'n': { x: 0, y: 1 },
-            'e': { x: 1, y: 0 },
-            's': { x: 0, y: -1 },
-            'w': { x: -1, y: 0 }
-        };
+        //console.log(directionMatrix);
 
         // check that direction is valid
-        if (this.directionMatrix[this.direction] == undefined) {
+        if (directionMatrix[this.direction] == undefined) {
             console.log('invalid constructor');
+            throw 'invalid constructor';
         }
 
         console.log();
@@ -37,22 +43,38 @@ var Robot = (function () {
         key: 'move',
         value: function move() {
             console.log('moving');
-            this.y += this.directionMatrix[this.direction].y;
-            this.x += this.directionMatrix[this.direction].x;
+            console.log(directionMatrix[this.direction]);
+            this.y += directionMatrix[this.direction].y;
+            this.x += directionMatrix[this.direction].x;
             console.log('x value ' + this.x);
             console.log('y value ' + this.y);
         }
     }, {
+        key: 'right',
+        value: function right() {
+            console.log('turning right from ' + this.direction);
+            var index = directionArray.indexOf(this.direction);
+            this.direction = directionArray[(index + 1) % 4];
+            console.log('new direction ' + this.direction);
+        }
+    }, {
         key: 'left',
         value: function left() {
-            console.log('turning left');
+            console.log('turning left from ' + this.direction);
+            var index = directionArray.indexOf(this.direction);
+            console.log('index' + index);
+            if (index == 0) index = 3;else index--;
+            this.direction = directionArray[index];
+            console.log('new direction ' + this.direction);
+        }
+    }, {
+        key: 'report',
+        value: function report() {
+            return { x: this.x, y: this.y };
         }
     }]);
 
     return Robot;
 })();
 
-var myRobot = new Robot(0, 1, 'north');
-var myNobot = new Robot(0, 1, 'dfverywhere');
-console.log(myRobot.x);
-myRobot.move();
+module.exports = Robot;
--- app/js/robot.jsx
+++ app/js/robot.jsx
@@ -1,3 +1,13 @@
+const directionMatrix = {
+            'n': {x: 0, y: 1},
+            'e': {x: 1, y: 0},
+            's': {x: 0, y: -1},
+            'w': {x: -1, y: 0}
+        };
+
+//todo: make this an iterator
+const directionArray = ['n','e','s','w'];
+
 class Robot
 {
     
@@ -8,16 +18,14 @@ class Robot
         
         this.direction = direction.charAt(0);
 
-        this.directionMatrix = { 
-            'n': {x: 0, y: 1},
-            'e': {x: 1, y: 0},
-            's': {x: 0, y: -1},
-            'w': {x: -1, y: 0}
-        };
+        //console.log(directionMatrix);
 
         // check that direction is valid
-        if (this.directionMatrix[this.direction] == undefined)
-         {console.log('invalid constructor')}
+        if (directionMatrix[this.direction] == undefined)
+        {
+            console.log('invalid constructor');
+            throw 'invalid constructor';
+        }
 
         console.log()
     }
@@ -25,23 +33,41 @@ class Robot
     get xCoordinate() {
         return this.x;
     }
+    
 
     move() {
         console.log('moving');
-        this.y += this.directionMatrix[this.direction].y
-        this.x += this.directionMatrix[this.direction].x
+        console.log(directionMatrix[this.direction])
+        this.y += directionMatrix[this.direction].y
+        this.x += directionMatrix[this.direction].x
         console.log('x value ' +this.x);
         console.log('y value ' +this.y);
     }
 
-    left() {
-        console.log('turning left');
+    right() {
+        console.log('turning right from ' + this.direction);
+        let index = directionArray.indexOf(this.direction);
+        this.direction = directionArray[(index+1) % 4];
+        console.log('new direction ' +this.direction);
+    }
 
+    left() {
+        console.log('turning left from ' + this.direction);
+        let index = directionArray.indexOf(this.direction);
+        console.log('index' + index);
+        if (index == 0)
+            index = 3;
+        else 
+            index--;
+        this.direction = directionArray[index];
+        console.log('new direction ' + this.direction);
+    }
+
+    report() {
+        return {x: this.x, y: this.y }
     }
 
 }
 
-var myRobot = new Robot(0,1,'north');
-var myNobot = new Robot(0,1,'dfverywhere');
-console.log(myRobot.x);
-myRobot.move();
+module.exports  = Robot;
+
